dea98cca57814edea94d7084df03362aa609ded2
DhominickJ <dhominickjohn.billena@wvsu.edu.ph>
2024-12-09 09:22:32 +0800

    Updated the Main Page

--- src/app/components/content-loader.tsx
+++ src/app/components/content-loader.tsx
@@ -28,9 +28,9 @@ function ContentLoader({
             height={100}
             className="left"
           />
-          <div className="pl-10 flex flex-col items-right justify-center w-1/2 text-white">
-            <h3 className="font-bold text-3xl pb-5">{header}</h3>
-            <p className="w-1/2 ">{content}</p>
+          <div className="pl-10 flex flex-col justify-center items-left w-1/2 text-white">
+            <h3 className="font-bold text-4xl pb-5">{header}</h3>
+            <p className="w-1/2 text-lg">{content}</p>
           </div>
         </div>
       )}
@@ -39,8 +39,8 @@ function ContentLoader({
           className={`w-full size-full h-500 p-32 flex items-center justify-center px-10 bg-${bgColor}`}
         >
           <div className="pl-10 flex flex-col justify-center items-left w-1/2">
-            <h3 className="font-bold text-3xl pb-5">{header}</h3>
-            <p className="w-1/2">{content}</p>
+            <h3 className="font-bold text-4xl pb-5">{header}</h3>
+            <p className="w-1/2 text-lg">{content}</p>
           </div>
           <Image
             src={imgSrc}
